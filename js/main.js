da79736cdc7cec1f64022a034a448712cc97e467
Michael Vasile <Michaelvas17@users.noreply.github.com>
2016-04-19 09:36:12 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-$(function() {
-    
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-  
-});
-
 function checkScroll(){
     var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
 
